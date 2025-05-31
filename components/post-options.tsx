ae173eb89b39504ac0394d9f7e9c104453e1a709
Enzo Bustamante <enzobustamante895@gmail.com>
2025-05-31 01:00:11 -0300

    feat: mejorar opciones de publicación con configuraciones de interacción y ajustes de diseño

--- components/post-options.tsx
+++ components/post-options.tsx
@@ -1,10 +1,5 @@
-import { GlobeIcon } from "lucide-react";
-import {
-  MorphingDialog,
-  MorphingDialogContainer,
-  MorphingDialogContent,
-  MorphingDialogTrigger,
-} from "./ui/morphing-dialog";
+import { useState } from "react";
+import { Check, GlobeIcon } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -14,66 +9,120 @@ import {
   CardTitle,
 } from "./ui/card";
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
 import {
   MorphingPopover,
   MorphingPopoverContent,
   MorphingPopoverTrigger,
 } from "./ui/morphing-popover";
+import { Toggle } from "./ui/toggle";
+import { Switch } from "./ui/switch";
 
 function PostOptions() {
+  const [replies, setReplies] = useState("everybody");
+  const [mentioned, setMentioned] = useState(false);
+  const [followed, setFollowed] = useState(false);
+  const [followers, setFollowers] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <MorphingPopover>
+    <MorphingPopover open={isOpen} onOpenChange={setIsOpen}>
       <MorphingPopoverTrigger asChild>
-        <p className="text-primary text-xs bg-muted-foreground/10 px-2 py-1 rounded-full hover:bg-secondary-foreground/20 cursor-pointer flex items-center gap-1">
-          {" "}
-          <GlobeIcon className="inline size-3 " />
+        <p
+          onClick={() => setIsOpen(true)}
+          className="text-primary text-xs bg-muted-foreground/10 px-2 py-1 rounded-full hover:bg-secondary-foreground/20 cursor-pointer flex items-center gap-1"
+        >
+          <GlobeIcon className="inline size-3" />
           Anybody can interact
         </p>
       </MorphingPopoverTrigger>
-      <MorphingPopoverContent className="z-50 fixed">
-        <Card className="w-[400px] max-w-sm max-h-3/4 ">
+      <MorphingPopoverContent className="z-50 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-md  rounded-lg shadow-lg ">
+        <Card className="w-full  max-h-3/4">
           <CardHeader>
-            <CardTitle>Create project</CardTitle>
+            <CardTitle>Post interaction settings</CardTitle>
             <CardDescription>
-              Deploy your new project in one-click.
+              Customize who can interact with this post.
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" placeholder="Name of your project" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="framework">Framework</Label>
-                  <Select>
-                    <SelectTrigger id="framework">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="next">Next.js</SelectItem>
-                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                      <SelectItem value="astro">Astro</SelectItem>
-                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
+            <div>
+              <div className="py-2 border-y-2 border-muted-foreground/20">
+                <p className="font-semibold">Quote settings</p>
+                <span className="flex items-center justify-between">
+                  <p>Allow quote posts</p>
+                  <Switch className="cursor-pointer" aria-readonly />
+                </span>
               </div>
-            </form>
+              <div className="pt-4">
+                <p className="font-semibold">Reply settings</p>
+                <p>Allow replies from:</p>
+                <span className="flex items-center justify-center gap-2">
+                  <Toggle
+                    className="cursor-pointer w-full justify-between"
+                    variant="outline"
+                    pressed={replies === "everybody"}
+                    onPressedChange={() => setReplies("everybody")}
+                  >
+                    Everybody
+                    {replies === "everybody" && (
+                      <Check className="inline size-3" />
+                    )}
+                  </Toggle>
+                  <Toggle
+                    className="cursor-pointer w-full justify-between"
+                    variant="outline"
+                    pressed={replies === "nobody"}
+                    onPressedChange={() => setReplies("nobody")}
+                  >
+                    Nobody
+                    {replies === "nobody" && (
+                      <Check className="inline size-3" />
+                    )}
+                  </Toggle>
+                </span>
+              </div>
+              {replies === "everybody" && (
+                <div className="flex flex-col gap-1 py-4">
+                  <p>or combine these options:</p>
+                  <Toggle
+                    className="cursor-pointer w-full justify-between"
+                    variant="outline"
+                    pressed={mentioned}
+                    onPressedChange={(val) => setMentioned(val)}
+                  >
+                    Mentioned users
+                    {mentioned && <Check className="inline size-3" />}
+                  </Toggle>
+                  <Toggle
+                    className="cursor-pointer w-full justify-between"
+                    variant="outline"
+                    pressed={followed}
+                    onPressedChange={(val) => setFollowed(val)}
+                  >
+                    Users you follow
+                    {followed && <Check className="inline size-3" />}
+                  </Toggle>
+                  <Toggle
+                    className="cursor-pointer w-full justify-between"
+                    variant="outline"
+                    pressed={followers}
+                    onPressedChange={(val) => setFollowers(val)}
+                    // onClick={(e) => e.stopPropagation()}
+                    // onClickCapture={(e) => e.stopPropagation()}
+                  >
+                    Your followers
+                    {followers && <Check className="inline size-3" />}
+                  </Toggle>
+                </div>
+              )}
+            </div>
           </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Deploy</Button>
+          <CardFooter className="flex justify-center items-center w-full">
+            <Button
+              onClick={() => setIsOpen(false)}
+              className="w-full cursor-pointer"
+            >
+              Save
+            </Button>
           </CardFooter>
         </Card>
       </MorphingPopoverContent>
