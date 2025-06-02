b78ee4cab3dd04335febc4e80d25df0590ca4377
Enzo Bustamante <enzobustamante895@gmail.com>
2025-06-01 23:48:12 -0300

    feat: agregar botón de cierre y mejorar la estructura del componente de opciones de publicación

--- components/post-options.tsx
+++ components/post-options.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { Check, GlobeIcon } from "lucide-react";
+import { Check, GlobeIcon, X } from "lucide-react";
 import { Button } from "./ui/button";
- import {
+import {
   Card,
   CardContent,
   CardDescription,
@@ -24,41 +24,63 @@ export default function PostOptions() {
   const [followers, setFollowers] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
-      <PopoverTrigger
-        asChild
-      >
-        <p  className="text-primary text-xs bg-muted-foreground/10 px-2 py-1 rounded-full hover:bg-secondary-foreground/20 cursor-pointer flex items-center gap-1">
-
- <GlobeIcon className="inline size-3" />
- Anybody can interact 
+      <PopoverTrigger asChild>
+        <p className="text-primary text-xs bg-muted-foreground/10 px-2 py-1 rounded-full hover:bg-secondary-foreground/20 cursor-pointer flex items-center gap-1">
+          <GlobeIcon className="inline size-3" />
+          Anybody can interact
         </p>
       </PopoverTrigger>
-      <PopoverContent className="w-screen z-50 fixed top-1/2 left-1/2  -translate-y-1/2 max-w-sm rounded-xl border bg-popover shadow-xl md:max-w-md max-h-dvw"  sideOffset={8}>
-        <Card className="overflow-hidden rounded-xl shadow-none border-0" onMouseDown={(e) => e.stopPropagation()}>
+      <PopoverContent
+        className="w-screen z-50 fixed top-1/2 left-1/2  -translate-y-1/2 max-w-sm rounded-xl border bg-popover shadow-xl md:max-w-md   max-h-[90vh] overflow-y-auto"
+        sideOffset={8}
+        onMouseDown={(e) => e.stopPropagation()}
+      >
+        <Card className="overflow-hidden rounded-xl shadow-none border-0">
           <CardHeader className="bg-muted/30 ">
-            <CardTitle className="text-lg font-semibold">Post Interaction Settings</CardTitle>
+            <CardTitle className="text-lg font-semibold">
+              Post Interaction Settings
+            </CardTitle>
             <CardDescription className="text-sm text-muted-foreground">
               Customize who can interact with this post.
             </CardDescription>
+            <Button
+              variant="ghost"
+              className="absolute top-2 right-2 p-1 cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            >
+              <X className="size-4" />
+            </Button>
           </CardHeader>
-          <CardContent >
+          <CardContent>
             <div className="space-y-1">
               <div className="space-y-3 rounded-lg border border-border bg-card p-4">
-                <p className="font-medium text-card-foreground">Quote Settings</p>
+                <p className="font-medium text-card-foreground">
+                  Quote Settings
+                </p>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="allow-quote-posts" className="text-sm text-muted-foreground">
+                  <label
+                    htmlFor="allow-quote-posts"
+                    className="text-sm text-muted-foreground"
+                  >
                     Allow quote posts
                   </label>
-                  <Switch id="allow-quote-posts" className="cursor-pointer" aria-readonly />
+                  <Switch
+                    id="allow-quote-posts"
+                    className="cursor-pointer"
+                    aria-readonly
+                  />
                 </div>
               </div>
 
               <div className="space-y-3 rounded-lg border border-border bg-card p-4">
-                <p className="font-medium text-card-foreground">Reply Settings</p>
-                <p className="text-sm text-muted-foreground">Allow replies from:</p>
+                <p className="font-medium text-card-foreground">
+                  Reply Settings
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  Allow replies from:
+                </p>
                 <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
                   <Toggle
                     className="w-full justify-between rounded-md border border-input bg-transparent px-4 py-2 text-sm font-medium shadow-sm hover:bg-accent hover:text-accent-foreground data-[state=on]:bg-primary data-[state=on]:text-primary-foreground  cursor-pointer"
@@ -83,7 +105,9 @@ export default function PostOptions() {
 
               {replies === "everybody" && (
                 <div className="space-y-3 rounded-lg border border-border bg-card p-4">
-                  <p className="text-sm font-medium text-muted-foreground">Or, refine who can reply:</p>
+                  <p className="text-sm font-medium text-muted-foreground">
+                    Or, refine who can reply:
+                  </p>
                   <div className="space-y-2">
                     <Toggle
                       className="w-full justify-between rounded-md border border-input bg-transparent px-4 py-2 text-sm font-medium shadow-sm hover:bg-accent hover:text-accent-foreground data-[state=on]:bg-accent data-[state=on]:text-accent-foreground  cursor-pointer"
